1079a16dc626f535e3f47d8121736244309184bb
patrizio.munzi <patrizio.munzi@piksel.com>
2015-10-06 12:17:44 +0200

    The rebuild-ssh-config-from-aws script wasn't moving to the GemFile folder before running the ruby script.

--- rebuild-ssh-config-from-aws.sh
+++ rebuild-ssh-config-from-aws.sh
@@ -4,10 +4,16 @@ if [ $# -lt 1 ]; then
 	echo "Usage: rebuild-ssh-config <profile-1> <profile-2> ..." 
 fi
 
+CUR_DIR=`pwd`
+EXEC_DIR=`dirname $0`
+cd $EXEC_DIR;
+
 for var in "$@"
 do
-	bundle exec ./ssh-servers-from-aws.rb "$var" > /dev/null 2>&1
+	bundle exec $EXEC_DIR/ssh-servers-from-aws.rb "$var" > /dev/null 2>&1
 done
+
+cd $CUR_DIR;
 
 ### Concat all the ssh config files into the main file
 ### WARNING: This will erase your current .ssh/config file! Move stuff you want to keep into an .sshconfig file in the ssh folder here.
