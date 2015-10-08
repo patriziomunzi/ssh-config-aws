032cf5af3ec9ce1df11a1265bbaa1106106f3ea7
patrizio.munzi <patrizio.munzi@piksel.com>
2015-10-08 15:44:34 +0200

    Added the possibility to configure which are the AWS tag name of the tags containing: - Name of the instance - Stage of the instance
    Added the possibility to configure Host and shuttle.name using a pattern which can leverage the following variables:
    - $NAME value of the tag with name NAME_TAG_NAME
    - $STAGE value of the tag with name STAGE_TAG_NAME
    - $ADDRESS ip address of the aws instance
    - $PROFILE profile name

--- ssh-servers-from-aws.rb
+++ ssh-servers-from-aws.rb
@@ -14,13 +14,33 @@ end
 
 access_key_id = config.match(/AWS_ACCESS_KEY=(.+)/)[1]
 secret_access_key = config.match(/AWS_SECRET_KEY=(.+)/)[1]
-identity_file = config.match(/IDENTITY_FILE=(.+)/)[1]
-skip_regexp = config.match(/SKIP_REGEXP=(.+)/)[1]
 
-# Add more regions here if necessary. No harm in adding all of them, just makes the generation take longer to query more regions.
-regions = [
-  'eu-west-1'
-]
+regions_matcher = config.match(/AWS_REGIONS=(.+)/)
+regions_str = regions_matcher ? regions_matcher[1] : ''
+
+regions = regions_str.split(",")
+if regions.length == 0
+  puts "No regions found for #{slug}"
+  exit
+end
+
+identity_file = config.match(/IDENTITY_FILE=(.+)/)[1]
+
+skip_regexp_matcher = config.match(/SKIP_REGEXP=(.+)/)
+skip_regexp = skip_regexp_matcher ? skip_regexp_matcher[1] : ''
+
+host_pattern_matcher = config.match(/HOST_PATTERN=(.+)/)
+host_pattern = host_pattern_matcher ? host_pattern_matcher[1] : '$NAME[$ADDRESS]'
+
+shuttlename_pattern_matcher = config.match(/SHUTTLE_NAME_PATTERN=(.+)/)
+shuttlename_pattern = shuttlename_pattern_matcher ? shuttlename_pattern_matcher[1] : '$SLUG/$STAGE/$NAME[$ADDRESS]'
+
+name_tag_name_matcher = config.match(/NAME_TAG_NAME=(.+)/)
+name_tag_name = name_tag_name_matcher ? name_tag_name_matcher[1] : 'Name'
+
+stage_tag_name_matcher = config.match(/STAGE_TAG_NAME=(.+)/)
+stage_tag_name = stage_tag_name_matcher ? stage_tag_name_matcher[1] : 'Stage'
+
 
 ssh_config = ''
 ssh_config << "\#============================================\n"
@@ -56,23 +76,44 @@ regions.each do |region|
 
     instance_name = nil
     instance.tags.each do |tag|
-      if(tag.key != 'Name')
+      if(tag.key != name_tag_name)
         next
       end
       instance_name = tag.value
     end
+    instance_stage = ''
+    instance.tags.each do |tag|
+      if(tag.key != stage_tag_name )
+        next
+      end
+      instance_stage = tag.value
+    end
 
     if(instance_name != nil && !instance_name.match(/#{skip_regexp}/) )
-      instance_name_tokens = instance_name.split("::")
+      #instance_name_tokens = instance_name.split("::")
       #instance_name = instance_name.gsub /[^a-zA-Z0-9\-_\.]/, '-'
       #instance_user = instance.tags['User'] || 'ubuntu'
       custom_user_regexp_res = config.match(/#{instance_name}\.USER=(.+)/)
       instance_user = custom_user_regexp_res ? custom_user_regexp_res[1] : 'ubuntu'
 
+      instance_ip_address = instance.private_ip_address
+
+      host = host_pattern
+      host = host.gsub /\$SLUG/, slug
+      host = host.gsub /\$NAME/, instance_name
+      host = host.gsub /\$ADDRESS/, instance_ip_address
+      host = host.gsub /\$STAGE/, instance_stage
+
+      shuttlename = shuttlename_pattern;
+      shuttlename = shuttlename.gsub /\$PROFILE/, slug
+      shuttlename = shuttlename.gsub /\$NAME/, instance_name
+      shuttlename = shuttlename.gsub /\$ADDRESS/, instance_ip_address
+      shuttlename = shuttlename.gsub /\$STAGE/, instance_stage
+
       #puts "#{instance.instance_id}: #{instance_name} #{instance.private_ip_address} (#{instance_user})"
-      ssh_config << "Host #{instance_name}[#{instance.private_ip_address}]\n"
-      ssh_config << "  # shuttle.name = #{slug}/#{instance_name_tokens[2]}/#{instance_name}[#{instance.private_ip_address}]\n"
-      ssh_config << "  HostName #{instance.private_ip_address}\n"
+      ssh_config << "Host #{host}\n"
+      ssh_config << "  # shuttle.name = #{shuttlename}\n"
+      ssh_config << "  HostName #{instance_ip_address}\n"
       ssh_config << "  User #{instance_user}\n"
       ssh_config << "  IdentityFile ~/.ssh/#{identity_file}\n"
       ssh_config << "\n"
